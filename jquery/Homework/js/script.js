36583b5d360130c22e608b07dfec8143075f9cff
Azat <aza.er.01@mail.ru>
2019-03-24 14:17:18 +0600

    40

--- jquery/Homework/js/script.js
+++ jquery/Homework/js/script.js
@@ -1,24 +1,46 @@
 $(document).ready(function() {
 	console.log(0);
 
-	$('a[href = "#sheldure"]').click(function() {
+	$('.modal').css('top', '-200');
+
+	$('a[href = "#sheldure"]').click(function(e) {
+		e.preventDefault();
+		$('.modal').animate({
+			top: 0
+		});
 		$('.modal').show();
 		$('.overlay').show();
+		
 	});
 
-	$('.main_btna').on('click', function() {
+	$('.main_btna').on('click', function(e) {
+		e.preventDefault();
+		$('.modal').animate({
+			top: 0
+		});
 		$('.modal').show();
 		$('.overlay').show();
+		
 	});
 
-	$('.main_btn').on('click', function() {
+	$('.main_btn').on('click', function(e) {
+		e.preventDefault();
+		$('.modal').animate({
+			top: 0
+		});
 		$('.modal').show();
 		$('.overlay').show();
+		
 	});
 
-	$('.close').on('click', function() {
-		$('.modal').hide();
-		$('.overlay').hide();
-	})
+	$('.close').on('click', function(e) {
+		e.preventDefault();
+		$('.modal').animate({
+			top: -200
+		});
+		$('.modal').hide('slow');
+		$('.overlay').hide('slow');
+		
+	});
 
 }); 
